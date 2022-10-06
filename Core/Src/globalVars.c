7d230866c5609d93acfa8559824e6ef4d1cf1732
radolini3 <radolini3@o2.pl>
2022-10-06 11:57:39 +0200

    działający build - wszystko analogowe

---
+++ Core/Src/globalVars.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "adc.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+	/*Moje include'y*/
+	#include "string.h"
+	#include "stdio.h"
+	#include "lcd_i2c.h"
+		 /*Zmienne przechowujące bufor dla przesyłania UART'em*/
+		 char UartOutText[100], UartInput[5];
+		 /*ADC zmienna*/
+		 uint32_t ADC_VAL[8];
+		 /*Zmienne do przeliczenia wartości z adc na procenty*/
+		 float moisture_percentage[6];
+		 /*Zmienne przechowujące odczyt czujnika temperatury*/
+		 float Temperature;
+		 float Humidity;
+		 /*Zmienna do przełączania wyświetlacza*/
+		 int disp_No = 1;
+		 /*Obsługa LCD*/
+		 struct lcd_disp disp;
+		 int count = 0;
+		 /*Zmienna do sterowania czasem między odczytami i przesyłem danych z cuzjników*/
+		 short sensorRead_freq = 1;
+		 short pushCount = 0;
---
+++ Core/Src/utilityCodes.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include "tim.h"
+#include "usart.h"
+/*Moje include'y*/
+	#include "globalVars.h"
+	#include "string.h"
+	#include "stdio.h"
+	#include "lcd_i2c.h"
+/*==============================Callbacki przerwań==============================*/
+	/*Przerwanie po upłynięciu tim6 równego 10000 cykli (po sekundzie) - zebranie danych z czujników - clock 72MHz prescaler 7200-1, cycles 10000*/
+	void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+
+		if ((count == sensorRead_freq) || (count == 0)){
+		analogDeviceReadDMA();
+		displayReadings(disp_No);
+		}
+		count++; //Dodawaj jeden co sekundę do tego countera
+		if (count >= sensorRead_freq) count = 1;
+		// Jeśli ilość sekund count jest większa od sensor_read_freq to zresetuj count do 1,
+		// pozwala to ustawić jedną zmienną częstotliwość odświeżania pomiarów ile tylko dusza zapragnie
+	}
+	/*Przerwanie na liniach 10-15, w tym przypadku to jest button na płytce Nucleo*/
+		void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+		{
+				if(!(HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin))){
+				disp_No++;
+				if (disp_No == 5) disp_No = 1;
+				displayReadings(disp_No);
+				}
+		}
+/*==========================================================================================*/
+	/*Milisekundowy delay na timerze, tim2 prescaler 72-1, brak trybu IT*/
+	void delay_us(uint32_t time){
+		__HAL_TIM_SET_COUNTER(&htim2, 0);
+		while ((__HAL_TIM_GET_COUNTER(&htim2))<time);
+	}
+
+	/*Wyślij po UARCIE numer 2 baudrate = 38400/s*/
+	void sendString_UART(char*text){
+		HAL_UART_Transmit(&huart2,  (uint8_t*)text, strlen(text), 1000);
+	}
+
+	/*Przeskaluj wartość analogową z ADCka na procenty*/
+	float map(uint16_t val, int in_min, int in_max, int out_min, int out_max) {
+	  return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	}
